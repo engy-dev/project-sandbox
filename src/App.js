72e0f71f4e579441b0a1d27c39a096b8304ed2e1
engy <engyessam70@gmail.com>
2024-12-02 19:27:42 +0100

    Wrapping up temperature control mini app

--- src/App.js
+++ src/App.js
@@ -1,16 +1,12 @@
 import "./App.css";
 /* import AlertApp from "./projects/05-Alerts/AlertApp"; */
-import TemperatureControlApp from "./projects/06-temperature-controller/TemperatureControlApp";
+/* import TemperatureControlApp from "./projects/06-temperature-controller/TemperatureControlApp"; */
 /* import TestimonialsApp from "./projects/04-testimonials-app/TestimonialsApp";
 import RandomizeColor from "./projects/02-color-randomizer/RandomizeColor"; */
 /* import LikePhotoApp from "./projects/03-like-my-pic/LikePhotoApp"; */
 /* import EsignatureApp from "./projects/01-e-signature-app/EsignatureApp"; */
 function App() {
-  return (
-    <div className="App">
-      <TemperatureControlApp />
-    </div>
-  );
+  return <div className="App"></div>;
 }
 
 export default App;
--- src/projects/06-temperature-controller/TemperatureControlApp.jsx
+++ src/projects/06-temperature-controller/TemperatureControlApp.jsx
@@ -1,10 +1,35 @@
-import React from "react";
+import React, { useState } from "react";
+import Button from "../components/Button";
 
 export default function TemperatureControlApp() {
+  const [temperature, setTemperature] = useState(0);
+  const increaseTemperature = () => {
+    setTemperature(temperature + 1);
+  };
+  const decreaseTemperature = () => {
+    setTemperature(temperature - 1);
+  };
+
   return (
     <div className="container mt-3 text-center">
-      <div className="card bg-light m-auto">
-        <h1>temperature </h1>
+      <div className="card bg-light m-auto" style={{ width: 200 }}>
+        <h1
+          className={`text-light card border-50 ${
+            temperature > 0 ? "bg-danger" : "bg-info"
+          }`}
+          style={{ height: 150, width: 150, border: "2px solid #666" }}
+        >
+          {temperature}°C{" "}
+        </h1>
+        {temperature <= 0 ? (
+          <p>damn jackie i can't change the weather</p>
+        ) : (
+          <p> Ummmm... what happens to snowmen in summer?</p>
+        )}
+        <div className="d-flex my-2">
+          <Button text="-" btnClass={"btn-lg"} onClick={decreaseTemperature} />
+          <Button text="+" btnClass={"btn-lg"} onClick={increaseTemperature} />
+        </div>
       </div>
     </div>
   );
